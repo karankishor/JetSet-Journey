1a2721b9f8dd531e2b44da285b1e4387984a7b82
Karan <89378732+karankishor@users.noreply.github.com>
2025-04-27 08:59:31 +0530

    Delete Auth directory

--- Auth/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import PasswordReset
-from .models import Trip, Itinerary
-
-admin.site.register(PasswordReset)
-admin.site.register(Trip)
-admin.site.register(Itinerary)
--- Auth/settings.py
+++
@@ -1,6 +0,0 @@
-# OpenWeather API Key
-OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY')
-
-# Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
